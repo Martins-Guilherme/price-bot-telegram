b1df4071a67569a0056cbec4f766a894de7ea078
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-04-12 04:08:09 -0300

    formatar a criação da tabela 'prices' no banco de dados

--- src/db/database.js
+++ src/db/database.js
@@ -7,16 +7,16 @@ const db = new Database(process.env.DATABASE_PATH || "/database/data.db");
 
 db.prepare(
   `
-    CREATE TABLE IF NOT EXISTS prices (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      product TEXT NOT NULL,
-      price REAL NOT NULL,
-      source TEXT NOT NULL,
-      image TEXT,
-      link TEXT,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-      )
-    `,
+  CREATE TABLE IF NOT EXISTS prices (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product TEXT NOT NULL,
+    price REAL NOT NULL,
+    source TEXT NOT NULL,
+    image TEXT,
+    link TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`,
 ).run();
 
 export default db;

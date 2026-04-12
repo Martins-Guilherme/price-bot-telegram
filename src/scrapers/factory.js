13cdbd0ca07c4fc877eb520570e8eab7dd8b80e2
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-04-12 04:08:52 -0300

    adicionar arquivo factory.js para gerenciar scrapers de diferentes plataformas

---
+++ src/scrapers/factory.js
@@ -0,0 +1,24 @@
+import { ScraperNotFoundError } from "../errors/index.js";
+
+import AmazonScraper from "./modules/amazonScraper.js";
+import KabumScraper from "./modules/kabumScraper.js";
+import MercadoLivreScraper from "./modules/mercadolivreScraper.js";
+
+const scraperTypes = {
+  amazon: AmazonScraper,
+  mercadolivre: MercadoLivreScraper,
+  kabum: KabumScraper,
+};
+
+export function getScraper(type) {
+  const scraperClass = scraperTypes[type?.toLowerCase()];
+
+  if (!scraperClass) {
+    throw new ScraperNotFoundError(`Tipo de busca "${type}", não encontrado.`);
+  }
+  return new scraperClass();
+}
+
+export function getAllScrapers() {
+  return Object.keys(scraperTypes).map((key) => new scraperTypes[key]());
+}

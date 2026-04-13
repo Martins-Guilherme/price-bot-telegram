f039d5b1248f4927c24d02fe1ef24728f25a319d
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-04-13 00:17:03 -0300

    ajustar valor padrão da coluna created_at para considerar o fuso horário GMT-3

--- src/db/database.js
+++ src/db/database.js
@@ -15,7 +15,7 @@ db.prepare(
     source TEXT NOT NULL,
     image TEXT,
     link TEXT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME DEFAULT (datetime('now', '-3 hours'))
   )
 `,
 ).run();

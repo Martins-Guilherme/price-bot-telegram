97382754fb250a30ee47edd3742c192ace057d57
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-04-16 06:25:43 -0300

    adicionar: mover a verificação de tipo de userId para o início da função canUse

--- src/utils/rateLimit.js
+++ src/utils/rateLimit.js
@@ -1,9 +1,19 @@
 const users = new Map();
-
 const COLLDOW = 10000; // 10 segundos
+
+import { BotRatLimitIsNotNumberError } from "../errors/index.js";
 
 export function canUse(userId) {
   const now = Date.now();
+  if (typeof userId !== "number") {
+    throw new BotRatLimitIsNotNumberError(
+      "userId deve ser um número",
+      typeof userId,
+      " (",
+      userId,
+      ")",
+    );
+  }
   const last = users.get(userId);
 
   if (last && now - last < COLLDOW) {

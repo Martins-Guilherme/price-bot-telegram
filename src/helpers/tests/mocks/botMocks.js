49b35b061a1099f69fc57f8927fb116e25396f49
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-04-17 01:52:49 -0300

    remover: excluir mock de bot duplicado

--- src/helpers/tests/mocks/botMocks.js
+++
@@ -1,7 +0,0 @@
-export const creteBotMock = () => {
-  return {
-    sendMessage: jest.fn().mockResolvedValue({}),
-    sendPhoto: jest.fn().mockResolvedValue({}),
-    deleteMessage: jest.fn().mockResolvedValue({}),
-  };
-};

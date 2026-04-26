46485eb47d641e4ddb2791f729a272bfd71bf554
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2026-04-26 01:55:27 -0300

    refactor(kabum-scraper): migrar de cheerio para puppeteer e adicionar medidas anti-bot

--- src/scrapers/modules/kabumScraper.js
+++ src/scrapers/modules/kabumScraper.js
@@ -1,39 +1,118 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { BaseScraper } from "../baseScraper.js";
+import { KabumScraperError } from "../../errors/index.js";
+import { getBrowser } from "../../utils/browser.js";
 
 class KabumScraper extends BaseScraper {
   name = "Kabum";
+  timeout = 30000;
   async search(productName) {
-    const url = `https://www.kabum.com.br/busca/${encodeURIComponent(productName)}`;
+    if (!productName) throw new KabumScraperError("Produto não informado");
 
-    const { data } = await axios.get(url, {
-      headers: {
-        "User-Agent": "Mozilla/5.0",
-      },
-    });
+    const baseURL = "https://www.kabum.com.br";
+    const slug = productName.trim().toLowerCase().replace(/\s+/g, "-");
 
-    const $ = cheerio.load(data);
-    const results = [];
+    const url = `${baseURL}/busca/${slug}`;
 
-    $(".productCard").each((_, el) => {
-      const title = $(el).find(".nameCard").text().trim();
-      const priceRaw = $(el).find(".priceCard").text().trim();
+    let browser = await getBrowser();
+    const context = await browser.createBrowserContext();
+    const page = await context.newPage();
+    // page.on("console", (msg) => console.log("KABUM PAGE:", msg.text()));
 
-      if (!title || !priceRaw) return;
-
-      const price = parseFloat(
-        priceRaw.replace("R$", "").replace(/\./g, "").replace(",", "."),
+    try {
+      await page.setJavaScriptEnabled(true);
+      await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
       );
 
-      results.push({
-        title,
-        price,
-        source: "kabum",
+      await page.setExtraHTTPHeaders({
+        "Accept-Language": "pt-BR,pt;q=0.9,en;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
       });
-    });
 
-    return results;
+      await page.setViewport({
+        width: 1366,
+        height: 768,
+      });
+
+      await page.evaluateOnNewDocument(() => {
+        Object.defineProperty(navigator, "webdriver", { get: () => false });
+        Object.defineProperty(navigator, "plugins", { get: () => [1, 2, 3] });
+        Object.defineProperty(navigator, "languages", {
+          get: () => ["pt-BR", "pt", "en"],
+        });
+        window.chrome = { runtime: {} };
+      });
+
+      await page.goto(url, {
+        waitUntil: "networkidle2",
+      });
+
+      // Lógica de Scroll Otimizada
+      await page.evaluate(async () => {
+        await new Promise((resolve) => {
+          let totalHeight = 0;
+          let distance = 400;
+          let maxScrolls = 15;
+          let scrolls = 0;
+
+          let timer = setInterval(() => {
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+            scrolls++;
+
+            if (
+              totalHeight >= document.body.scrollHeight ||
+              scrolls >= maxScrolls
+            ) {
+              clearInterval(timer);
+              resolve();
+            }
+          }, 400);
+        });
+      });
+
+      const items = await page.evaluate(() => {
+        const results = [];
+
+        document
+          .querySelectorAll('a[href^="/produto/"][aria-label]')
+          .forEach((el) => {
+            const aria = el.getAttribute("aria-label") || "";
+
+            const title = aria.split(", avaliação")[0]?.trim() || "Sem título";
+
+            const priceMatch = aria.match(/R\$\s*([\d.,]+)/);
+
+            const price = priceMatch ? parseFloat(priceMatch[1]) : NaN;
+
+            const image =
+              el.querySelector("img")?.src ||
+              el.querySelector("img")?.getAttribute("data-src") ||
+              "";
+
+            const link = `https://www.kabum.com.br` + el.getAttribute("href");
+
+            results.push({
+              title,
+              price,
+              image,
+              link,
+              source: "kabum",
+            });
+          });
+
+        return results.slice(0, 3);
+      });
+      return items;
+    } catch (error) {
+      console.error("Erro no scraper: ", error);
+      return [];
+    } finally {
+      if (page && !page.isClosed()) await page.close();
+      if (browser) await browser.close();
+    }
   }
 }
 
